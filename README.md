3ed63609c338af476c48900dcfb92a49f68fc980
Callum Rollo <c.rollo@outlook.com>
2025-04-28 21:36:56 +0200

    f1rst post

---
+++ README.md
@@ -0,0 +1,7 @@
+#
+21:05 make repo
+21:07 copy the leaflet quick-start example
+21:10-21:26 why is it so hard to read local files into javascript?
+21:27 give up and just write geojson to a .js file like they do in the leaflet geojson demo (lol)
+21:20 add a manual example route
+21:31 realise this should be a flask python app or something
---
+++ routes.js
@@ -0,0 +1,21 @@
+var routes = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [11.9, 57.7],
+                    [2.3, 48.8]
+                ]
+            },
+            "properties": {
+                "popupContent": "I'm told you can get from Gothenburg to Paris in a couple of days",
+                "underConstruction": false
+            },
+            "id": 1
+        }
+    ]
+};
+
